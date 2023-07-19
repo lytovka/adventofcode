601a15e74f681048e49bf59a15208932d2f689a4
ivanlytovka <ivanlytovka@gmail.com>
2023-07-19 10:50:49 -0400

    fix: standardize names

---
+++ 2022/day1/part_2.js
@@ -0,0 +1,25 @@
+import path from "path";
+import fs from "fs";
+
+const input = fs.readFileSync(path.resolve("input.txt"), "utf-8");
+
+const groups = input.trim().split("\n\n");
+
+console.log(groups);
+
+const groupsAccumulatedCalories = groups.map((group) =>
+  group
+    .split("\n")
+    .map((s) => parseInt(s, 10))
+    .reduce((acc, cal) => acc + cal)
+);
+
+const groupsAccumulatedCaloriesSortedDesc = groupsAccumulatedCalories.sort(
+  (a, b) => b - a
+);
+
+console.log(
+  groupsAccumulatedCaloriesSortedDesc[0] +
+    groupsAccumulatedCaloriesSortedDesc[1] +
+    groupsAccumulatedCaloriesSortedDesc[2]
+);
