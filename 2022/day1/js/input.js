0577262288c0f066cea223534348d4501b47bb8d
ivanlytovka <ivanlytovka@gmail.com>
2023-09-03 19:05:03 -0400

    move

--- 2022/day1/js/input.js
+++ 2022/day1/js/input.js
@@ -4,6 +4,6 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const input = fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf-8");
+const input = fs.readFileSync(path.resolve(__dirname, "../input.txt"), "utf-8");
 
 export default input;
