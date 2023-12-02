359be62a8556589d07756d66546a27b8811609dc
ivanlytovka <ivanlytovka@gmail.com>
2023-12-01 23:54:59 -0500

    2023 day1 in js

---
+++ 2023/day1/js/part_2.js
@@ -0,0 +1,26 @@
+import input from "./input.js"
+
+const stringToDigit = {
+	"one": "1",
+	"two": "2",
+	"three": "3",
+	"four": "4",
+	"five": "5",
+	"six": "6",
+	"seven": "7",
+	"eight": "8",
+	"nine": "9"
+}
+
+const splitRegex = /(one|two|three|four|five|six|seven|eight|nine|\d)/g
+
+const columns = input.trim().split("\n")
+
+const numbers = columns.map(c => {
+	const numbers = c.match(splitRegex).map(value => stringToDigit[value] ?? value)
+	return parseInt(numbers.at(0) + numbers.at(-1))
+})
+
+const sum = numbers.reduce((acc, n) => acc + n)
+
+console.log(sum)
