bb45bc71d315b4b4c2a355cebb23a676a22f255a
ivanlytovka <ivanlytovka@gmail.com>
2023-12-06 22:48:32 -0500

    solve day6

---
+++ 2023/day6/js/part_1.js
@@ -0,0 +1,21 @@
+import {input} from "./input.js"
+
+const [times, distances] = input.trim().split("\n").map(line => line.match(/\d+/g).map(Number))
+
+console.log(times, distances)
+
+let counts = Array.from({length: times.length}).fill(0)
+
+for(let i = 0; i < times.length; i++){
+	let time = times[i]
+	let distance = distances[i]
+	
+	for(let j = 1; j < time; j++){
+		const localDistance = j * (time - j)
+		if(localDistance > distance){
+			counts[i] += 1; 
+		}
+	}
+}
+
+console.log(counts.reduce((acc, val) => acc * val, 1))
