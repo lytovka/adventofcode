b5d34df2dccaec15447b4923d60eaffd9efddd08
ivanlytovka <ivanlytovka@gmail.com>
2023-12-09 16:22:16 -0500

    solve day9

---
+++ 2023/day9/js/part_2.js
@@ -0,0 +1,25 @@
+import {input1, input} from "./input.js"
+
+const histories = input.trim().split("\n").map(line => line.split(" ").map(Number))
+console.log(histories)
+
+const getDiff = (arr) => {
+	let diff = []
+	for(let i = 1; i < arr.length; i++){
+		diff.push(arr[i] - arr[i-1])
+	}
+	return diff
+}
+
+const findPreviousValue = (history) => {
+	if(history.every(value => value === history[0])){
+		return history[0]
+	}
+
+	const diff = getDiff(history)
+	const nextDiff = findPreviousValue(diff)
+	return history.at(0) - nextDiff
+}
+
+console.log(histories.map(findPreviousValue).reduce((acc, val) => acc + val, 0))
+
