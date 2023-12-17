106fb320f8c1b73c2b0ecd8a38069aeab686c25c
ivanlytovka <ivanlytovka@gmail.com>
2023-12-17 15:39:16 -0500

    solve day16

--- 2023/day16/js/part_2.js
+++ 2023/day16/js/part_2.js
@@ -7,7 +7,7 @@ import crypto from "node:crypto"
  * @param {number[][]} initPos
  */
 const getEnergizedTiles = (contraption, initPos) => {
-	const beamsMap = new Map([[initPos.toString(), initPos.toString()]])
+	const beamsMap = new Map([[crypto.randomUUID(), initPos.toString()]])
 	const energizedTiles = new Map()
 	for (const [id, value] of beamsMap){
 		const [bRow, bCol, dRow, dCol] = value.split(",").map(Number)
@@ -161,10 +161,42 @@ const getEnergizedTiles = (contraption, initPos) => {
 		}
 	}
 
-	return energizedTiles
+	return energizedTiles.size
 }
 
 const rows = input.trim().split("\n")
 
-console.log(getEnergizedTiles(rows, [[0,0], [1,0]]).size)
+let max = Number.MIN_SAFE_INTEGER
+
+// top to bottom and bottom to top
+for (let i = 0; i < rows.length; i++) {
+	max = Math.max(
+		max,
+		getEnergizedTiles(rows, [
+			[0, i],
+			[1, 0],
+		]),
+		getEnergizedTiles(rows, [
+			[rows[0].length - 1, i],
+			[-1, 0],
+		])
+	);
+}
+
+// left to right and right to left 
+for (let i = 0; i < rows[0].length; i++) {
+	max = Math.max(
+		max,
+		getEnergizedTiles(rows, [
+			[i, 0],
+			[0, 1],
+		]),
+		getEnergizedTiles(rows, [
+			[i, rows.length - 1],
+			[0, -1],
+		])
+	);
+}
+
+console.log(max)
 
