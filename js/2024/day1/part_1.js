8509e7a2bfbbfea34d374fbcb0dda4309ca4b518
ivanlytovka <ivanlytovka@gmail.com>
2025-02-16 03:32:12 -0500

    move

--- js/2024/day1/part_1.js
+++ js/2024/day1/part_1.js
@@ -1,12 +1,12 @@
-import input from "./input.js";
+import { readInputFromFile } from "../../utils/readInputFromFile.js"
+
+const input = readInputFromFile(2024, 1);
 
 const lines = input.trim().split("\n");
 const numbers = lines.map((line) => line.split(/\s+/g).map(Number));
 
 const left = numbers.map((row) => row[0]).sort((a, b) => a - b);
 const right = numbers.map((row) => row[1]).sort((a, b) => a - b);
-
-console.log(left.length, right.length);
 
 const result = left.reduce((acc, left, i) => {
   return acc + Math.abs(left - right[i]);
