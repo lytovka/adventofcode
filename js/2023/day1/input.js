7ee6adb5236b2c188c1f94102fdf4f29789a61c6
ivanlytovka <ivanlytovka@gmail.com>
2025-02-16 14:25:18 -0500

    delete local input.js files

--- js/2023/day1/input.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const input = fs.readFileSync(path.resolve(__dirname, "../input.txt"), "utf-8");
-
-export default input;
