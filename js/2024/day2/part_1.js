0a5cac9f999c207d2d006111ba5b41b359e7218d
ivanlytovka <ivanlytovka@gmail.com>
2025-02-17 15:08:13 -0500

    solve day2 part2

--- js/2024/day2/part_1.js
+++ js/2024/day2/part_1.js
@@ -1,25 +1,28 @@
 import { readInputFromFile } from "~/utils/readInputFromFile.js";
 
 const getOrder = (num1, num2) => {
-	return num1 > num2 ? "d" : num1 < num2 ? "i" : "n"
-}
+  return num1 > num2 ? "d" : num1 < num2 ? "i" : "n";
+};
 
 const input = readInputFromFile(2024, 2);
 const rows = input.split("\n").map((row) => row.split(" ").map(Number));
 
 let result = 0;
 rows.forEach((row) => {
-	let save = true
-	const order = getOrder(row[0], row[1])
-	if (order === "n") return;
-	for (let i = 0; i < row.length - 1; i++) {
-		if (Math.abs(row[i] - row[i + 1]) > 3 || getOrder(row[i], row[i + 1]) !== order) {
-			save = false
-		}
-	}
-	if (save) {
-		result += 1;
-	}
+  let save = true;
+  const order = getOrder(row[0], row[1]);
+  if (order === "n") return;
+  for (let i = 0; i < row.length - 1; i++) {
+    if (
+      Math.abs(row[i] - row[i + 1]) > 3 ||
+      getOrder(row[i], row[i + 1]) !== order
+    ) {
+      save = false;
+    }
+  }
+  if (save) {
+    result += 1;
+  }
 });
 
 console.log(result);
---
+++ js/2024/day2/part_2.js
@@ -0,0 +1,36 @@
+import { readInputFromFile } from "~/utils/readInputFromFile.js";
+
+const input = readInputFromFile(2024, 2);
+
+const isSafe = (sequence) => {
+  const asc = new Set([1, 2, 3]);
+  const desc = new Set([-1, -2, -3]);
+
+  for (let i = 1; i < sequence.length; i++) {
+    let res = sequence[i] - sequence[i - 1];
+    asc.add(res);
+    desc.add(res);
+  }
+  return asc.size === 3 || desc.size === 3;
+};
+
+const rows = input.split("\n").map((row) => row.split(" ").map(Number));
+
+let result = 0;
+
+for (const row of rows) {
+  let tolerable = false;
+  for (let i = 0; i < row.length; i++) {
+    const arr = [...row.slice(0, i), ...row.slice(i + 1)];
+
+    if (isSafe(arr)) {
+      tolerable = true;
+      break;
+    }
+  }
+  if (isSafe(row) || tolerable) {
+    result += 1;
+  }
+}
+
+console.log(result);
