4f3907471f2497f293c61fa8a130acf3e4bf8c0b
ivanlytovka <ivanlytovka@gmail.com>
2025-02-17 13:13:32 -0500

    solve day2 part1

--- js/2024/day2/part_1.js
+++ js/2024/day2/part_1.js
@@ -1,5 +1,25 @@
 import { readInputFromFile } from "~/utils/readInputFromFile.js";
 
-const input = readInputFromFile(2024, 2);
+const getOrder = (num1, num2) => {
+	return num1 > num2 ? "d" : num1 < num2 ? "i" : "n"
+}
 
-console.log(input);
+const input = readInputFromFile(2024, 2);
+const rows = input.split("\n").map((row) => row.split(" ").map(Number));
+
+let result = 0;
+rows.forEach((row) => {
+	let save = true
+	const order = getOrder(row[0], row[1])
+	if (order === "n") return;
+	for (let i = 0; i < row.length - 1; i++) {
+		if (Math.abs(row[i] - row[i + 1]) > 3 || getOrder(row[i], row[i + 1]) !== order) {
+			save = false
+		}
+	}
+	if (save) {
+		result += 1;
+	}
+});
+
+console.log(result);
