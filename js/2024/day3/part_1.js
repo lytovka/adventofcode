fd81356924aa6c5155f692458fe952c5454f4c69
ivanlytovka <ivanlytovka@gmail.com>
2025-02-19 19:43:20 -0500

    solve day3 part1

---
+++ js/2024/day3/part_1.js
@@ -0,0 +1,13 @@
+import { readInputFromFile } from "~/utils/readInputFromFile.js";
+
+const input = readInputFromFile(2024, 3);
+
+const matches = input.match(/mul\((\d+),(\d+)\)/g);
+const parseMatches = matches.map((match) =>
+  match
+    .match(/mul\((\d+),(\d+)\)/)
+    .slice(1)
+    .map(Number),
+);
+
+console.log(parseMatches.reduce((acc, [a, b]) => acc + a * b, 0));
