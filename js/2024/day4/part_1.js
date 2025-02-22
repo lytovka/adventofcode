28187290fe021ce7ae6a32fe0bfdfb4f796e67b8
ivanlytovka <ivanlytovka@gmail.com>
2025-02-22 12:38:52 -0500

    refactor

--- js/2024/day4/part_1.js
+++ js/2024/day4/part_1.js
@@ -9,58 +9,33 @@ let result = 0;
 
 function readTo(input, x, y) {
   function east() {
-    return input[x][y] + input[x][y + 1] + input[x][y + 2] + input[x][y + 3];
+    return concat([x, y], [x, y + 1], [x, y + 2], [x, y + 3]);
   }
   function south() {
-    return input[x][y] + input[x + 1][y] + input[x + 2][y] + input[x + 3][y];
+    return concat([x, y], [x + 1, y], [x + 2, y], [x + 3, y]);
   }
   function west() {
-    return input[x][y] + input[x][y - 1] + input[x][y - 2] + input[x][y - 3];
+    return concat([x, y], [x, y - 1], [x, y - 2], [x, y - 3]);
   }
   function north() {
-    return input[x][y] + input[x - 1][y] + input[x - 2][y] + input[x - 3][y];
+    return concat([x, y], [x - 1, y], [x - 2, y], [x - 3, y]);
   }
   function northEast() {
-    return (
-      input[x][y] +
-      input[x - 1][y + 1] +
-      input[x - 2][y + 2] +
-      input[x - 3][y + 3]
-    );
+    return concat([x, y], [x - 1, y + 1], [x - 2, y + 2], [x - 3, y + 3]);
   }
   function northWest() {
-    return (
-      input[x][y] +
-      input[x - 1][y - 1] +
-      input[x - 2][y - 2] +
-      input[x - 3][y - 3]
-    );
+    return concat([x, y], [x - 1, y - 1], [x - 2, y - 2], [x - 3, y - 3]);
   }
   function southEast() {
-    return (
-      input[x][y] +
-      input[x + 1][y + 1] +
-      input[x + 2][y + 2] +
-      input[x + 3][y + 3]
-    );
+    return concat([x, y], [x + 1, y + 1], [x + 2, y + 2], [x + 3, y + 3]);
   }
   function southWest() {
-    return (
-      input[x][y] +
-      input[x + 1][y - 1] +
-      input[x + 2][y - 2] +
-      input[x + 3][y - 3]
-    );
+    return concat([x, y], [x + 1, y - 1], [x + 2, y - 2], [x + 3, y - 3]);
   }
 
-  function concat(coords) {
-    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = coords
-    return (
-      input[x1][y1] +
-      input[x2][y2] +
-      input[x3][y3] +
-      input[x4][y4]
-    );
+  function concat(...coords) {
+    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = coords;
+    return input[x1][y1] + input[x2][y2] + input[x3][y3] + input[x4][y4];
   }
 
   return {
@@ -77,12 +52,10 @@ function readTo(input, x, y) {
 
 for (let i = 0; i < input.length; i++) {
   for (let j = 0; j < input.length; j++) {
-    console.log(i, j);
     Object.values(readTo(input, i, j)).forEach((fn) => {
       try {
         const value = fn();
         if (acceptedAnswers.has(value)) {
-          console.log(value, i, j, fn);
           result += 1;
         }
       } catch (ex) {
