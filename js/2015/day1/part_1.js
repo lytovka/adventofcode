cadcf6313c6ed89b018d3a9fa06fd8209e5d0630
ivanlytovka <ivanlytovka@gmail.com>
2025-05-02 18:31:48 -0400

    2015 part 1 and 2

--- js/2015/day1/part_1.js
+++ js/2015/day1/part_1.js
@@ -1,10 +1,10 @@
 import { readInputFromFile } from "~/utils/readInputFromFile.js";
 
 const input = readInputFromFile(2015, 1);
-const chars = input.trim().split("")
-let res = 0
-chars.forEach(char => {
-    if(char === "(") res++
-    if(char === ")") res--
-})
+const chars = input.trim().split("");
+let res = 0;
+chars.forEach((char) => {
+  if (char === "(") res++;
+  if (char === ")") res--;
+});
 console.log(res);
