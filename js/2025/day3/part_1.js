77e61d91abe7dbfc20ed4a5862d43bca2c599588
Ivan Lytovka <ivanlytovka@gmail.com>
2025-12-06 16:04:23 -0500

    2025 day3 part1

---
+++ js/2025/day3/part_1.js
@@ -0,0 +1,28 @@
+import { readInputFromFile } from "../../utils/misc.js";
+
+const input = readInputFromFile(2025, 3);
+const input2 = `
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+`
+
+const banks = input.trim().split("\n")
+
+const maxs = []
+for (let i = 0; i < banks.length; i++) {
+  const bank = banks[i]
+  let max = -1
+  for (let j = 0; j < bank.length; j++) {
+    for (let z = j + 1; z < bank.length; z++) {
+      const val = parseInt(bank[j] + bank[z])
+      if (val > max) {
+        max = val
+      }
+    }
+  }
+  maxs.push(max)
+}
+
+console.log(maxs.reduce((acc, val) => acc + val, 0));
